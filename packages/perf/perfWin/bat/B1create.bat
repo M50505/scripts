9cf50cdc8a05a52ba36c0cf211a53901af805c8d
Martin Cribbins <M50505@stonefifteen.com>
2025-09-29 10:51:25 -0400

    Added text to _doc calls that were missing descriptive text.

--- packages/perf/perfWin/bat/B1create.bat
+++
@@ -1,1 +0,0 @@
-logman create counter B1 -f bincirc -max 500 -si 1 --v -o "K:\shared\data\B1\B1" -cf "K:\shared\data\B1\B1.config" 
--- packages/perf/perfWin/bat/perfWinCreateDCS.bat
+++
@@ -1,1 +0,0 @@
-logman create counter B1 -f bincirc -max 500 -si 1 -o E:\perfcap.blg -v mmddhhmm -ow -cf B1.config
